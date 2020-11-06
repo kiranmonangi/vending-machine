0ce70f433e014f712f072cda8fed20675111276e
kiranmonangi <kiranmonangi1998@gmail.com>
2020-11-06 18:17:51 +0530

    first commit

---
+++ src/com/vending/Customer.java
@@ -0,0 +1,7 @@
+package com.vending;
+
+public class Customer {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/vending/Rough.java
@@ -0,0 +1,14 @@
+package com.vending;
+
+import java.io.Console;
+
+public class Rough
+{
+    public static void main(String[] args)
+    {
+        // Using Console to input data from user
+        String name = System.console().readLine();
+
+        System.out.println(name);
+    }
+}
