2ebee6096b82f2e8d49808d3f2a22b0030269649
kiranmonangi <74052076+kiranmonangi@users.noreply.github.com>
2020-11-06 20:04:44 +0530

    first commit having owner and vendingitems files

---
+++ EzeuProject/src/com/company/VendingItems.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class VendingItems {
+    public String itemname;
+    public int price,quantity;
+
+    public VendingItems(){
+
+    }
+    public VendingItems(String itemname,int price,int quantity){
+        super();
+        this.itemname=itemname;
+        this.price=price;
+        this.quantity=quantity;
+
+    }
+
+    @Override
+    public String toString() {
+        return "vending items[item name="+itemname+"  price="+price+ "  quantity:"+quantity+"]";
+    }
+}
+
